43c176133e9075151e8fa3cde92906d8eab11912
Faysal155 <1330929@iub.edu.bd>
2017-10-10 00:27:29 +0600

    Assignment 7

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,29 @@
 // Your jQuery goes here
 
 var userData = {};
+name:"",
+email:"",
+html:{likes}:[], dislikes:[]},
+css:{likes}[], dislikes:[]},
+js:{likes}:[], dislikes[]},
+strengths: {html}:"", css:"",js:""},
+currentQuestion: "welcome"
+
+
+
+$("startBtn").click(function)(event){
+$("welcome").hide();
+$("#q1").show();
+
+});
+$("name").change(function)(event){
+	console.long($("name").val());
+});
+
+$("#email").change(function(event)){
+console.log($("#email").val());
+if ("#email").val() !="" && $("#name").val())
+$("#q1Next").prop("disable",false)};
+)};
+
+$("#q1Next").click(function(event){userData.new=$("#name")})
